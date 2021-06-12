dc85774643315ae0bb9b3daff768c44fcb3bbc26
yonePC <ys.mailaddress0929@gmail.com>
2021-06-12 08:03:05 +0000

    [update]kadai2-16.17

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def new
+    @book = Book.new
+  end
+  
+  def create
+     book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+    
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
