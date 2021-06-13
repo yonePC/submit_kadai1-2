1b6cab0a52819880d060b540f556fcb16bbf16cc
yonePC <ys.mailaddress0929@gmail.com>
2021-06-13 12:27:38 +0000

    [update]kadai2-23

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,32 +10,32 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(book.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-end  
+
+end
