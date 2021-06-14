15a6df9be9b898ecd7fcac3142788a10fad5cd61
yonePC <ys.mailaddress0929@gmail.com>
2021-06-14 08:52:28 +0000

    [update]フラッシュとバリエーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+    
   end
 
   def show
@@ -12,9 +14,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @books = Book.all
+    
+    if @book.save
+     redirect_to book_path(@book.id)
+     flash[:notice] = "Book was successfully created."
+    else
+     render :index
+    end
   end
 
   def edit
@@ -24,13 +32,16 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
+    
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
   end
 
   private
