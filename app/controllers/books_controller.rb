6918875336395a6665afd5bf6da90f855cef8dfc
yonePC <ys.mailaddress0929@gmail.com>
2021-06-15 04:55:13 +0000

    [submit]kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-    
+
   end
 
   def show
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @books = Book.all
-    
+
     if @book.save
      redirect_to book_path(@book.id)
      flash[:notice] = "Book was successfully created."
@@ -30,11 +30,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+
+    if @book.save
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
-    
+    redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
